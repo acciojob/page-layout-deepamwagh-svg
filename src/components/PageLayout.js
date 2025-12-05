18d4c7d1bb9a115e7357bf687758e376ca2f1db6
Deepam Wagh <deepam.wagh@gmail.com>
2025-12-05 13:40:41 +0530

    solve

---
+++ src/components/PageLayout.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function PageLayout({ header, footer, children }) {
+    return (
+        <div>
+            <header>{header}</header>
+
+            <main>{children}</main>
+
+            <footer>{footer}</footer>
+        </div>
+    );
+}
